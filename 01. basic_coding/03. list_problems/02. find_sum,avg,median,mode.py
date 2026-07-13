29869e5887222907cb1b1fb1f2bd1c1742a24146
Athithya Krishnaa M <athithya0006@gmail.com>
2026-07-13 13:34:38 +0530

    Update 02. find_sum,avg,median,mode.py

--- 01. basic_coding/03. list_problems/02. find_sum,avg,median,mode.py
+++ 01. basic_coding/03. list_problems/02. find_sum,avg,median,mode.py
@@ -1,41 +1,35 @@
-# find sum, average, median, mode of array elements
+# sum, avg, median, mode
 
-arr=[12, 7, 5, 7, 18, 20, 12, 7, 25, 30, 18, 18, 5]
-sum=0
-i=0
+arr = [1, 2, 3, 4]
 n=len(arr)
+sum=0
 
-# sum
-while i<n:
-    sum+=arr[i]
-    i+=1
+for num in arr:
+    sum+=num
 
-# average
-avg=sum//n
+avg=sum/n
 
-# median
-arr_sort=sorted(arr)
+arr_srt=sorted(arr)
 if n%2!=0:
-    median=arr_sort[n//2]
+    median=arr_srt[n//2]
 else:
-    median=(arr_sort[(n//2)-1]+arr_sort[n//2])//2
-    
-# mode
+    median=(arr_srt[n//2-1]+arr_srt[n//2])/2
 
 hm={}
 for num in arr:
     hm[num]=hm.get(num,0)+1
-max_count=0
-mode=arr[0]
 
-for key, value in hm.items():
-    if value>max_count:
-        max_count=value
-        mode=key
-        
-print("sum       :",sum)
-print("average   :",avg)
-print("median    :",median)
-print("mode      :",mode)
+max_value=float('-inf')
+for key,value in hm.items():
+    if value>max_value:
+        max_value=value
+mode=[]
+for key,value in hm.items():
+    if value==max_value:
+        mode.append(key)
+    
 
-
+print('Sum     :',sum)
+print('Average :',avg)
+print('Median  :',median)
+print('Mode    :',mode)
