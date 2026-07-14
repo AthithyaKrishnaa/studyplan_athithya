2fe3e7c91a90ff044cc7f1b25181d0ef07fe5455
Athithya Krishnaa <athithya0006@gmail.com>
2026-07-14 13:39:54 +0530

    add java problems

---
+++ 02. company_studyplan/cognizant_digital_nurture_studyplan/inheritance,method_overload&override.java
@@ -0,0 +1,36 @@
+// Inheritance, Method Overloading and Method Overriding
+
+class Main extends Animal{
+    public static void main(String[] args){
+        // inheritance
+        dog();
+        // method overload
+        add(10,20);
+        add(10.5,20.5);
+        // method override
+        Main obj = new Main();
+        obj.cat();
+    }
+    // @Override
+    void cat(){
+            System.out.println("meow");
+    }
+}
+
+class Animal{
+    static void dog(){
+        System.out.println("bow");
+    }
+    static void add(int a, int b){
+        System.out.println(a+b);
+    }
+    static void add(double a, double b){
+        System.out.println(a+b);
+    }
+    void cat(){
+        System.out.println("animal");
+    }
+}
+
+
+
