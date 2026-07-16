ae6f0be94cc967bf3cb5749f48cf8ecb7a53d04a
Athithya Krishnaa <athithya0006@gmail.com>
2026-07-16 11:15:50 +0530

    add collections

---
+++ 02. company_studyplan/cognizant_digital_nurture_studyplan/study/collections/listt.java
@@ -0,0 +1,46 @@
+// add, remove, search, iterate, sort
+import java.util.*;
+class listt{
+    public static void main(String[] args){
+        // array list 
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        System.out.println("Array List :");
+        arr.add(30);
+        arr.add(20);
+        arr.add(10);
+        
+        arr.remove(Integer.valueOf(20));
+        
+        System.out.println(arr.contains(10));
+        
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        Collections.sort(arr);
+        System.out.println(arr);
+        System.out.println();
+
+        // linked list
+        System.out.println("Linked List :");
+        
+        LinkedList<Integer> arr2 = new LinkedList<>();
+        
+        arr2.add(30);
+        arr2.add(20);
+        arr2.add(10);
+        
+        arr2.remove(Integer.valueOf(20));
+        
+        System.out.println(arr2.contains(10));
+        
+        for(int i: arr2){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        Collections.sort(arr2);
+        System.out.println(arr2);
+    }
+    
+}
---
+++ 02. company_studyplan/cognizant_digital_nurture_studyplan/study/collections/sett.java
@@ -0,0 +1,77 @@
+// add, remove, search, iterate, sort list
+import java.util.*;
+class sett{
+    public static void main(String[] args){
+        // hash set 
+        
+        HashSet<Integer> set = new HashSet<>();
+        System.out.println("HashSet :");
+        set.add(30);
+        set.add(20);
+        set.add(10);
+        set.add(99);
+        set.add(2);
+        
+        set.remove(Integer.valueOf(20));
+        
+        System.out.println(set.contains(10));
+        
+        for(int i: set){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        
+        ArrayList<Integer> arr = new ArrayList<>(set);
+        Collections.sort(arr);
+        System.out.println(arr);
+        System.out.println();
+
+        // hash set 
+        
+        LinkedHashSet<Integer> set2 = new LinkedHashSet<>();
+        System.out.println("Linked HashSet :");
+        set2.add(30);
+        set2.add(20);
+        set2.add(10);
+        
+        set2.remove(Integer.valueOf(20));
+        
+        System.out.println(set2.contains(10));
+        
+        for(int i: set2){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        
+        ArrayList<Integer> arr2 = new ArrayList<>(set2);
+        Collections.sort(arr2);
+        System.out.println(arr2);
+        System.out.println();
+        
+        // tree set
+        
+        System.out.println("Tree Set :");
+        
+        TreeSet<Integer> set3 = new TreeSet<>();
+        
+        set3.add(30);
+        set3.add(20);
+        set3.add(10);
+        set3.add(99);
+        set3.add(2);
+        
+        set3.remove(Integer.valueOf(20));
+        
+        System.out.println(set3.contains(10));
+        
+        for(int i: set3){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        
+        //Collections.sort(set3);
+        System.out.println(set3);
+        
+    }
+    
+}
